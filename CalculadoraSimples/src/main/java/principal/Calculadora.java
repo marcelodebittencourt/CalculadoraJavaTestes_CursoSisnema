6473702664ef1edfac2fb1ccd3c561d65e56ed61
SS3004 <SS3004@INSTRUTOR6>
2020-08-15 15:40:28 -0300

    Calculadora com soma e subtracao com testes unitarios

---
+++ CalculadoraSimples/src/main/java/principal/Calculadora.java
@@ -0,0 +1,13 @@
+package principal;
+
+public class Calculadora {
+	
+	public int soma(int x, int y) {
+		return x+y;
+	}
+	
+	public int substracao(int x, int y) {
+		return x-y;
+	}
+
+}
---
+++ CalculadoraSimples/src/main/java/principal/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Calculadora calc = new Calculadora();
+		
+		System.out.println(calc.soma(3, 2));
+
+	}
+
+}
